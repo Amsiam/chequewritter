96b9d673d87ef6913a7c50ceb3241447e68cb8f7
Amsiam <amsiam990@gmail.com>
2025-06-21 21:25:08 +0600

    feat: Implement user management functionality with role assignment and user status

---
+++ database/migrations/2025_06_21_151105_add_role_in_users.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('role')->default('user')->after('email')->comment('Role of the user, e.g., admin, user, etc.');
+            $table->boolean('is_active')->default(true)->after('role')->comment('Indicates if the user is active or not');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['role', 'is_active']);
+        });
+    }
+};
